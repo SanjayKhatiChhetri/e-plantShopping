cfba95ddcbf50175e7975921979360ac25dbc962
Sanjay Khati Chhetri <sanjaykhati579@gmail.com>
2024-08-15 16:25:49 +0300

    Update script and styles for E-Plant website

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,27 +5,29 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       <h3 className="about-us-description">
-        " Discover the Beauty of <span style={{ color: "#4caf50" }}>Green</span>{" "}
-        "
+        Welcome to Paradise Nursery, where green meets serenity!
       </h3>
       <p className="about-us-content">
-        At Green Galores, we are passionate about bringing nature closer to you.
-        Our mission is to provide a wide range of high-quality plants that not
-        only enhance the beauty of your surroundings but also contribute to a
-        healthier and more sustainable lifestyle.
+        At Paradise Nursery, we are passionate about bringing nature closer to
+        you. Our mission is to provide a wide range of high-quality plants that
+        not only enhance the beauty of your surroundings but also contribute to
+        a healthier and more sustainable lifestyle. From air-purifying plants to
+        aromatic fragrant ones, we have something for every plant enthusiast.
       </p>
 
       <p className="about-us-content">
         Our team of experts is dedicated to ensuring that each plant meets our
         strict standards of quality and care. Whether you're a seasoned gardener
         or just starting your green journey, we're here to support you every
-        step of the way.
+        step of the way. Feel free to explore our collection, ask questions, and
+        let us help you find the perfect plant for your home or office.
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
 
       <p className="about-us-content">
-        Join us in our mission to create a greener,{" "}
-        <span style={{ color: "#ab47bc" }}>colorful</span> world.
+        Join us in our mission to create a greener, healthier world. Visit
+        Paradise Nursery today and experience the beauty of nature right at your
+        doorstep.
       </p>
     </div>
   );
